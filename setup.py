4972f26e9a08e8273958a3513bb9e92c29eba94e
alexmjn <a.jenkneary@gmail.com>
2020-02-10 16:51:24 -0500

    edit setup

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="ajn-lambdata-package",
-    version="1",
+    version="0.0.1",
     author="Alex JN",
-    #author_email="datacreativellc@gmail.com",
-    #description="Gameplay logic for Rock-Paper-Scissors",
+    author_email="ajenkneary@gmail.com",
+    description="Gameplay logic for Rock-Paper-Scissors",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     license="MIT",
-    #url="https://github.com/s2t2/game-utils-py",
-    #keywords="rock paper scissors game",
+    url="https://github.com/alexmjn/ajn-lambdata-package",
     packages=find_packages() # ["game_utils"]
 )
