09e698508cf4ac31de6892ba1ff2eec615ff3025
alexmjn <a.jenkneary@gmail.com>
2020-02-11 11:29:33 -0500

    add functions

--- ajn_lambdata_package/ajn_module.py
+++ ajn_lambdata_package/ajn_module.py
@@ -1,2 +1,6 @@
-def funcname(parameter_list):
-    pass
+def check_nulls(df):
+    return(df.isnull().sum().sort_values())
+
+def add_col(df, new_list, colname):
+    df[colname] = pd.Series(new_list)
+    return df
