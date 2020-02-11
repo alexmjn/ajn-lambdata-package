364b50df34c6b74775b057cd5470989121ec9686
alexmjn <a.jenkneary@gmail.com>
2020-02-11 15:05:42 -0500

    Added pypirc file. Still trying to submit to TestPyPI

--- build/lib/ajn_lambdata_package/ajn_module.py
+++ build/lib/ajn_lambdata_package/ajn_module.py
@@ -1,2 +1,24 @@
-def funcname(parameter_list):
-    pass
+def check_nulls(df):
+    """
+    Takes a data frame and returns a sorted list of the number
+    of null values each column has.
+    """
+    return(df.isnull().sum().sort_values())
+
+def add_col(df, new_list, colname):
+    import pandas as pd
+    """
+    Adds a user-provided list as a column to a user-provided data frame.
+
+    Takes three arguments: the data frame, the list to be appended, and
+    the name of the new column. Converts the list into a series, then
+    defines a new column by the column name as the list-based series.
+
+    Params:
+
+        df (data frame): the data frame to be added to
+        new_list (list): the list you want to append to the df
+        colname (string): what you want to name the column
+    """
+    df[colname] = pd.Series(new_list)
+    return df
